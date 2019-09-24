6dee13c157b54622c6b49e4b54258762ae7167da
Amos Welike <amoswelike@gmail.com>
2019-09-24 19:42:57 +0300

    add files

---
+++ list_detail_view/src/models/UserStore.js
@@ -0,0 +1,51 @@
+import {
+    observable,
+    action,
+    computed,
+    decorate
+} from 'mobx';
+import axios from 'axios';
+
+class UserStore {
+     // First we place all of our observers
+    users = [];
+    selectedUser = {};
+    get selectedId() {
+        return this.selectedUser.id;
+    }
+    // In strict mode, only actions can modify mobx state
+    setUsers = (users) => {
+        this.users = [...users];
+    }
+    selectUser = (user) => {
+        this.selectedUser = user;
+    }
+    // Managing how we clear our observable state
+    clearSelectedUser = () => {
+        this.selectedUser = {};
+    }
+    // An example that's a little more complex
+    getUsers() {
+        //Managing Async tasks like ajax calls with Mobx actions
+        axios.get('http://jsonplaceholder.typicode.com/users').then(response => {
+            this.setUsers(response.data);
+        });
+    }
+}
+
+decorate(UserStore, {
+    users: observable,
+    selectedUser: observable,
+    selectedId: computed,
+    setUsers: action,
+    selectUser: action,
+    clearSelectedUser: action,
+    getUsers: action
+});
+
+const store = new UserStore();
+
+export default store;
+export {
+    UserStore
+};
